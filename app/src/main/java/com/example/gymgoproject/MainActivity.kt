71224f29447cb21003371e0b7007afabfe3eeb28
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-05-28 22:12:23 -0300

    implementation of home-destination navigation on the user input screen

--- app/src/main/java/com/example/gymgoproject/MainActivity.kt
+++ app/src/main/java/com/example/gymgoproject/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.gymgoproject.ui.screens.SignInScreen
+import com.example.gymgoproject.ui.states.SignInUiState
 import com.example.gymgoproject.ui.theme.GymgoProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GymgoProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "signIn"
+                ) {
+                    composable("signIn") {
+                        SignInScreen(uiState = SignInUiState())
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GymgoProjectTheme {
-        Greeting("Android")
     }
 }
